51f2b9e52ab150f6b4673520195dec1477c7d9fd
Ilham Akbar Pradana <ilhamakbar.pradana@gmail.com>
2022-10-06 00:03:10 +0700

    First Commit

---
+++ app/src/main/java/com/ace/uidesignexample/FirstFragment.kt
@@ -0,0 +1,36 @@
+package com.ace.uidesignexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ace.uidesignexample.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnToSecond.setOnClickListener{
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+}
---
+++ app/src/main/java/com/ace/uidesignexample/SecondFragment.kt
@@ -0,0 +1,37 @@
+package com.ace.uidesignexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ace.uidesignexample.databinding.FragmentFirstBinding
+import com.ace.uidesignexample.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnToFirst.setOnClickListener{
+            findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+    }
+}
